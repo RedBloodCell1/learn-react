16ad2aeacf98e30b0c717709bf96c45b5a606149
Martin Jason Alfredo <martinjason2212@gmail.com>
2023-05-16 17:20:54 +0700

    friendlist

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//CSS
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//SETUP VARS
+
+const friends = [
+  {
+    img: "https://i.ytimg.com/an_webp/6jraQOeWOqM/mqdefault_6s.webp?du=3000&sqp=CN6IjaMG&rs=AOn4CLDEX16K3Xy6ENXWmO0XxbBv4QCKZA",
+    name: "Martio",
+    skinColor: "Black",
+  },
+  {
+    img: "https://i.ytimg.com/an_webp/b6ozunDfahc/mqdefault_6s.webp?du=3000&sqp=CN6NjaMG&rs=AOn4CLBOZpYvFlLRjY71kRLoMXz4G-uZ5Q",
+    name: "Defriantio",
+    skinColor: "Brown",
+  },
+  {
+    img: "https://i.ytimg.com/vi/sPL-GybO1cA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDIDVrX6Q2oVHm5LbQa4UD4cRYcVQ",
+    name: "Jason",
+    skinColor: "Wheat",
+  },
+];
+
+//Rules
+// return single element
+// fragment <React.fragment> / section / div
+// camel case for html attribute
+// className instead of class
+// close every element
+
+function FriendList() {
+  return (
+    <section className="friendlist">
+      <Friend
+        img={firstFriend.img}
+        name={firstFriend.name}
+        skinColor={firstFriend.skinColor}
+      ></Friend>
+
+      <Friend
+        img={secondFriend.img}
+        name={secondFriend.name}
+        skinColor={secondFriend.skinColor}
+      ></Friend>
+      <Friend
+        img={thirdFriend.img}
+        name={thirdFriend.name}
+        skinColor={thirdFriend.skinColor}
+      ></Friend>
+    </section>
+  );
+}
+
+const Friend = (props) => {
+  const { img, name, skinColor } = props;
+  return (
+    <article className="friend">
+      <img src={img} alt="" />
+      <h1>{name}</h1>
+      <h4>{skinColor}</h4>
+    </article>
+  );
+};
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "BODOBOM");
+// };
+
+ReactDom.render(<FriendList />, document.getElementById("root"));
