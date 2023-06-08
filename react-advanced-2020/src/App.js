77a3498a283047a7812bf0415b25874140359bc7
Martin Jason Alfredo <martinjason2212@gmail.com>
2023-06-08 16:20:52 +0700

    useEffect

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/my-own-project";
-import Setup1 from "./tutorial/1-useState/setup/1-error-example";
-import Setup2 from "./tutorial/1-useState/setup/2-useState-basics";
-import Setup3 from "./tutorial/1-useState/setup/3-useState-array";
-import Setup4 from "./tutorial/1-useState/setup/4-useState-object";
-import Setup5 from "./tutorial/1-useState/setup/5-useState-counter";
+// import Setup from "./tutorial/1-useState/setup/my-own-project";
+// import Setup1 from "./tutorial/1-useState/setup/1-error-example";
+// import Setup2 from "./tutorial/1-useState/setup/2-useState-basics";
+// import Setup3 from "./tutorial/1-useState/setup/3-useState-array";
+// import Setup4 from "./tutorial/1-useState/setup/4-useState-object";
+// import Setup5 from "./tutorial/1-useState/setup/5-useState-counter";
+// import Setup6 from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+// import Setup7 from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
+// import Setup8 from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Setup9 from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
 function App() {
   return (
     <div className="container">
-      <Setup></Setup>
+      <Setup9 />
     </div>
   );
 }
--- react-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  const checkSize = () => {
+    setWidth([window.innerWidth]);
+    setHeight([window.innerHeight]);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>Width: {width} PX</h2>
+      <h2>Height: {height} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
